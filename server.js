9e5c84a23ba3557b3df20e033fcc31a771584cec
neomaxzero <maxicespedes7@hotmail.com>
2017-04-04 22:54:48 -0300

    adding custom server

---
+++ server.js
@@ -0,0 +1,24 @@
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next')
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+const server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+const server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+app.prepare()
+.then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true)
+    const { pathname, query } = parsedUrl
+
+    handle(req, res, parsedUrl)
+  })
+  .listen(server_port, (err) => {
+    if (err) throw err
+    console.log(`> Ready on http://${server_ip_address}:${server_port}`)
+  })
+})
