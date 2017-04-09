34aff792775de0af6ba5bbb4fdd5a7936f10dc71
neomaxzero <maxicespedes7@hotmail.com>
2017-04-08 22:22:37 -0300

    modify button and adding hover effect to search

--- components/shared/Button/ButtonSc.js
+++ components/shared/Button/ButtonSc.js
@@ -3,10 +3,14 @@ import { primary } from '~/components/shared/MainColors';
 
 export const ButtonSc = styled.div`
   color: white;
-  font-size: 1.4em;
-  padding: 0.5em 1em;
+  font-size: 1.2em;
+  padding: 0.6em 1.2em;
   border-radius: 4px
   background-color: ${ primary }
   cursor: pointer;
   user-select: none;
+
+  &:hover {
+    opacity: 0.8;
+  }
 `;
