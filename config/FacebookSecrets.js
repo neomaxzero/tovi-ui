54d4c1a7b2e3914535c574b92df34a212e34553b
neomaxzero <maxicespedes7@hotmail.com>
2017-04-19 23:43:59 -0300

    added config facebook

---
+++ config/FacebookSecrets.js
@@ -0,0 +1,6 @@
+export default {
+  appId      : '503078586529442',
+  cookie     : true,  // enable cookies to allow the server to access 
+  xfbml      : true,  // parse social plugins on this page
+  version    : 'v2.8' // use graph api version 2.8
+}
