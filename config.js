8032be2ecbe81da88d2b694c51c40269bcc4d06e
neomaxzero <maxicespedes7@hotmail.com>
2017-07-20 17:54:09 -0300

    fix domain api and id cookie

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const prod = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  'BASE_API': prod ? 'https://toviservicios.azurewebsites.net/' : 'http://localhost:8882/',
+  'BASE_API': 'https://toviservicios.azurewebsites.net/',
   'PRODUCTION': process.env.NODE_ENV === 'production',
   'BASE_DOMAIN': '.tovi.com.ar',
 }
--- utils/cookies.js
+++ utils/cookies.js
@@ -1,8 +1,12 @@
 import Cookies from 'js-cookie';
 
 export default {
-    set: (prop, value, expiration) => {
+    set: (prop, value, expires, domain = BASE_DOMAIN) => {
         //Expiration should be in days or parsed in some way.
-        return Cookies.set(prop, value, expiration);
+        const options = {
+            expires,
+            domain
+        }
+        return Cookies.set(prop, value, options);
     }
 }
