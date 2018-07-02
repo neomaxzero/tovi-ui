ca9d57f119a0509fe6112af3dd0900979245f819
Max <maxicespedes7@hotmail.com>
2018-07-02 20:07:18 +0200

    cards basic

--- .babelrc.js
+++ .babelrc.js
@@ -1,8 +1,8 @@
 module.exports = {
   presets: ['next/babel'],
   plugins: [
+    ['styled-components', { ssr: true, displayName: true, preprocess: false }],
     ['transform-define', require('./config.js')],
     ['babel-plugin-root-import'],
-    ['styled-components', { ssr: true, displayName: true, preprocess: false }],
   ],
 };
--- pages/_document.js
+++ pages/_document.js
@@ -1,33 +1,56 @@
-import React from 'react'
+import React from 'react';
 
-import Document, { Head, Main, NextScript } from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import Document, { Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 // import styleSheet from 'styled-components/lib/models/StyleSheet'
 
 class MyDocument extends Document {
-  render () {
+  static getInitialProps({ renderPage }) {
     const sheet = new ServerStyleSheet();
-    const main = sheet.collectStyles(<Main />);
+    const page = renderPage(App => props =>
+      sheet.collectStyles(<App {...props} />),
+    );
     const styleTags = sheet.getStyleElement();
+    console.log(styleTags);
+    return { ...page, styleTags };
+    console.log(styleTags);
+  }
 
+  render() {
     return (
       <html>
-        <Head>          
-          <link href="https://fonts.googleapis.com/css?family=Montserrat:400" rel="stylesheet" />
-          <link href="https://fonts.googleapis.com/css?family=Lato:300,400" rel="stylesheet" />              
-          <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />          
-          <link rel="stylesheet" href="https://unpkg.com/react-select/dist/react-select.css" />
+        <Head>
+          {this.props.styleTags}
+          <link
+            href="https://fonts.googleapis.com/css?family=Montserrat:400"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css?family=Lato:300,400"
+            rel="stylesheet"
+          />
+          <link
+            href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+            rel="stylesheet"
+          />
+          <link
+            rel="stylesheet"
+            href="https://unpkg.com/react-select/dist/react-select.css"
+          />
           <meta name="theme-color" content="#41C8C8" />
-          {styleTags}
         </Head>
         <body>
-          {main}
+          <Main />
           {this.props.customValue}
           <NextScript />
-          <script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit" async defer></script>
+          <script
+            src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit"
+            async
+            defer
+          />
         </body>
       </html>
-    )
+    );
   }
 }
 
